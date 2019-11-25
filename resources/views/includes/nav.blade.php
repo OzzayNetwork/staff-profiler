5909eef7b075720748db3d79acf29228b7f52ab0
ozzaynetworks <ozzaynetworks@gmail.com>
2019-11-25 12:44:51 +0300

    add user on the navigation's bottom

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -4,8 +4,10 @@
             <li class="card-link"><a href="/posts/" data-content="fellow members profiles">Staff profiles</a></li>
             <li><a href="/announcements" data-content="Office related announcements">announcements</a></li>
             <li><a href="/announcements" data-content="Upcoming events and staff birthdays">events</a></li>
+            @if(Auth::user()->is_admin==1)
             <li><a href="/users/create" data-content="Register a new account">create account</a></li>
-            <li><a class="dropdown-item" data-content="log me out" href="{{ route('logout') }}"
+            @endif
+            <li><a data-content="log me out" href="{{ route('logout') }}"
                onclick="event.preventDefault();
                              document.getElementById('logout-form').submit();">
                 {{ __('Logout') }}
@@ -22,7 +24,7 @@
 
 <div class="">
     <div class="position-absolute">
-        <a class="nav-prof-img" href="#" title="My Profile">
+    <a class="nav-prof-img" href="{{route('posts.show',Auth::id())}}" title="My Profile">
             @if(Auth::user()->post)
             <div class="img-container"><img src="/cover_images/{{Auth::user()->post->pic}}"></div>
             @endif
@@ -38,4 +40,7 @@
 <span class="bar-middle"></span>
 <span class="bar-bottom"></span>
 </div>
+@if(Auth::user()->is_admin==1)
+<span class="add-user position-fixed mb-5 mr-3"><a href="/users/create" title="Add A User" class="text-white"><i data-feather="user-plus"></i></a></span>
+@endif
  <div class="pb-60 w-100 pri-background"></div>
