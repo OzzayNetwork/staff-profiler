30a5dbf339df64b7939b8c4dfa5bf8fe9a7c26e1
Wambui Mwangi <swambuimwangi@gmail.com>
2019-11-25 14:52:16 +0300

    sends the new user mail on mail

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = 'posts/';
 
     /**
      * Create a new controller instance.
