201beb212fec525090becadcaab2a595c054a809
ozzaynetworks <ozzaynetworks@gmail.com>
2019-12-02 10:36:44 +0300

    index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,56 +1,14 @@
-@extends('layouts.app')
-   @section('content')
-   <section class="container-fluid the-container">
-		<div class="row m-5 text-capitalize the-profile-table">
-			<div class="col-12">
-				<h3 class="text-capitalize">the team</h3>
-				<p>below is your fellow workmeates, click on any to have a look at their profile</p>
-				<hr>
-				<table id="example" class="table  p-3 profile-table mt-3" style="width:100%">
-        <thead>
-            <tr class="d-none">
-                <th></th>
-            </tr>
-        </thead>
-        	<tbody>
-                @if(count($posts ?? '')>0)
-                    @foreach($posts ?? '' as $post)
-                         <tr>
-                            <td class="d-flex align-items-center">
-                                <a href="new_member.html">
-                                    <img src="../img/116838.jpg" class="img rounded-circle mr-3">
-                                    </a>
-                                <div>
-                                    <h6 class="p-0 m-0 text-capitalize">{{$post->name}}</h6>
-                                    <p class="text-uppercase mb-0 pb-0">{{$post->title}}</p>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
+@extends('layouts.create-app')
+@section('content')
+<div class="container-fluid the-container2 p-5 ">
+    <div class="row justify-content-center align-items-center  w-100">
+        <div class="col-sm-11 col-md-10 col-lg-10">
+            <h2 class="text-white text-left">The Nouveta Family Network</h2>
+            <p>Click on login to continue</p>
+            <span class="text-capitalize button underline-text"><a href="/login" class="text-white">Login To continue</a></span>
+        </div>
+    </div>
+</div>
 
-                @else
-                    <p>no profiles found</p>
-                @endif
-			<a class="dropdown-item" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
 
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-            </form>
-			</tbody>
-            </table>
-            </div>
-			
-		</div>
-	</section>
-	
-    @endsection
-
-    <script>
-            $(document).ready(function() {
-        $('#example').DataTable();
-    } );
-        </script>
+@endsection
